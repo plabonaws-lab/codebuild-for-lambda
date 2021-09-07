8395c131dd99cc5528770c1b2f3ccdf4ffad0208
plabonaws-lab <89953961+plabonaws-lab@users.noreply.github.com>
2021-09-07 11:00:55 +0600

    Create lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!. Just Now Completed CI/CD using Github & Lambda')
+        'body': json.dumps('Hello from Lambda!. Just Now Completed CI/CD using Github & Lambda, Code will Change automatically')
     }
